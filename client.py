a58112d6e3d6a355b31efecd30a73b153921d940
Luka Konovalov <konovluka@gmail.com>
2025-04-16 13:19:23 +0300

    Implemented server and client part, using hash to check the origin of message and RSA to encrypt, decrypt messages and safely communicate.

--- client.py
+++ client.py
@@ -1,5 +1,11 @@
 import socket
 import threading
+# importing library for hashing
+from hashlib import sha256
+
+from utils import (decode_public_key, decrypt_message, encode_public_key,
+                   encrypt_message, make_key_pair)
+
 
 class Client:
     def __init__(self, server_ip: str, port: int, username: str) -> None:
@@ -17,38 +23,46 @@ class Client:
 
         self.s.send(self.username.encode())
 
-        # create key pairs
+        self.public_key, self.private_key = make_key_pair()
+        encoded_public = encode_public_key(self.public_key)
 
-        # exchange public keys
+        self.server_public_key = decode_public_key(self.s.recv(1024))
+        self.s.send(encoded_public)
 
-        # receive the encrypted secret key
-
-        message_handler = threading.Thread(target=self.read_handler,args=())
+        message_handler = threading.Thread(target=self.read_handler, args=())
         message_handler.start()
-        input_handler = threading.Thread(target=self.write_handler,args=())
+        input_handler = threading.Thread(target=self.write_handler, args=())
         input_handler.start()
 
-    def read_handler(self): 
+    def read_handler(self):
         while True:
-            message = self.s.recv(1024).decode()
+            message = self.s.recv(1024)
+            hash_256_sent = message[:32]
+            encrypted_message = message[32:]
 
-            # decrypt message with the secrete key
-
-            # ... 
-
-
-            print(message)
+            decrypted_message = decrypt_message(
+                self.public_key, self.private_key, encrypted_message
+            )
+            hash_256 = sha256(decrypted_message)
+            if hash_256.digest() == hash_256_sent:
+                print(decrypted_message.decode())
+            else:
+                print("message might be tampered (hash mismatch)")
 
     def write_handler(self):
         while True:
             message = input()
+            message = "User " + '"' + self.username + '"' + " : " + message
+            hash_256 = sha256(message.encode())
 
-            # encrypt message with the secrete key
+            encrypted_message = encrypt_message(
+                self.server_public_key, message.encode()
+            )
+            hash_with_message = hash_256.digest() + encrypted_message
 
-            # ...
+            self.s.send(hash_with_message)
 
-            self.s.send(message.encode())
 
 if __name__ == "__main__":
-    cl = Client("127.0.0.1", 9001, "b_g")
+    cl = Client("127.0.0.1", 9002, "b1")
     cl.init_connection()
--- server.py
+++ server.py
@@ -1,60 +1,67 @@
 import socket
 import threading
+from hashlib import sha256
+
+from utils import (decode_public_key, decrypt_message, encode_public_key,
+                   encrypt_message, make_key_pair)
+
 
 class Server:
 
     def __init__(self, port: int) -> None:
-        self.host = '127.0.0.1'
+        self.host = "127.0.0.1"
         self.port = port
         self.clients = []
+        self.public_key_lookup = {}
         self.username_lookup = {}
-        self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def start(self):
         self.s.bind((self.host, self.port))
         self.s.listen(100)
 
         # generate keys ...
-
+        self.public_key, self.private_key = make_key_pair()
         while True:
             c, addr = self.s.accept()
             username = c.recv(1024).decode()
             print(f"{username} tries to connect")
-            self.broadcast(f'new person has joined: {username}')
+            self.broadcast(f"new person has joined: {username}")
             self.username_lookup[c] = username
             self.clients.append(c)
 
-            # send public key to the client 
+            c.send(encode_public_key(self.public_key))
+            client_public_key = decode_public_key(c.recv(1024))
 
-            # ...
+            self.public_key_lookup[c] = client_public_key
 
-            # encrypt the secret with the clients public key
-
-            # ...
-
-            # send the encrypted secret to a client 
-
-            # ...
-
-            threading.Thread(target=self.handle_client,args=(c,addr,)).start()
+            threading.Thread(
+                target=self.handle_client,
+                args=(
+                    c,
+                    addr,
+                ),
+            ).start()
 
     def broadcast(self, msg: str):
-        for client in self.clients: 
+        hash_256 = sha256(msg.encode())
+        for client in self.clients:
+            message = encrypt_message(self.public_key_lookup[client], msg.encode())
+            message = hash_256.digest() + message
+            client.send(message)
 
-            # encrypt the message
-
-            # ...
-
-            client.send(msg.encode())
-
-    def handle_client(self, c: socket, addr): 
+    def handle_client(self, c: socket, addr):
         while True:
             msg = c.recv(1024)
-
+            hash_256 = msg[:32]
+            msg = msg[32:]
+            msg = decrypt_message(self.public_key, self.private_key, msg)
             for client in self.clients:
                 if client != c:
-                    client.send(msg)
+                    encrypted = encrypt_message(self.public_key_lookup[client], msg)
+                    client.send(hash_256 + encrypted)
+
 
 if __name__ == "__main__":
-    s = Server(9001)
+    s = Server(9002)
     s.start()
